641be62f7dfb2d0af4c71927c0d52126b23ee71e
abrhammihiret <abrhammihiret1690@gmail.com>
2022-05-16 12:03:30 -0700

    commet

--- resources/views/Catagory/get_all.blade.php
+++ resources/views/Catagory/get_all.blade.php
@@ -9,7 +9,7 @@
  <div class="col-md-8">
  <div class="card">
  <div class="card-header">
- {{ __('ABREHAM THIS IS YOUR PRODUCT LIST') }}
+ {{ __('This is my product list. ') }}
  </div>
  <div class="card-body">
  <table class="table table-striped table-bordered ">
